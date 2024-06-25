c3e7d53f1966a71c14c54519155a99582b9c4125
Usmonjonjon <usmonjona007@gmail.com>
2024-06-25 19:31:57 +0500

    Confirm passworddi qildm

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,53 +1,73 @@
-import { Input , Form, Button } from 'antd'
+import { Input, Form, Button } from 'antd';
 import React from 'react';
-import "./Register.scss"
+import "./Register.scss";
 import { register } from '../../services/authService';
 
-
 const Register = () => {
-
   const [form] = Form.useForm();
 
-  const sendData  = async (e) => {
+  const sendData = async (values) => {
     try {
-      const data = await register(e)
+      const data = await register(values);
       console.log(data);
     } catch (error) {
       console.log(error);
     }
   }
-  
+
+  const confirmPasswordValidator = ({ getFieldValue }) => ({
+    validator(_, value) {
+      if (!value || getFieldValue('password') === value) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('The two passwords that you entered do not match!'));
+    },
+  });
+
   return (
     <div className='register'>
       <div className='register-box'>
-      <h2 className='register-title'>Register</h2>
+        <h2 className='register-title'>Register</h2>
 
-      <Form from={form} onFinish={sendData}>
-        <Form.Item name={'userName'}>
-          <Input placeholder='Username' />
-        </Form.Item>
-        <Form.Item name={'firstName'}>
-          <Input placeholder='First Name' />
-        </Form.Item>
-        <Form.Item name={'lastName'}>
-          <Input placeholder='Last Name' />
-        </Form.Item>
-        <Form.Item name={'email'}>
-          <Input placeholder='Email' />
-        </Form.Item>
-        <Form.Item name={'password'}>
-          <Input placeholder='Password' />
-        </Form.Item>
-        <Form.Item>
-          <Input placeholder='Confirm Password' />
-        </Form.Item>
+        <Form form={form} onFinish={sendData}>
+          <Form.Item name='userName'>
+            <Input placeholder='Username' />
+          </Form.Item>
+          <Form.Item name='firstName'>
+            <Input placeholder='First Name' />
+          </Form.Item>
+          <Form.Item name='lastName'>
+            <Input placeholder='Last Name' />
+          </Form.Item>
+          <Form.Item name='email'>
+            <Input placeholder='Email' />
+          </Form.Item>
+          <Form.Item name='password'>
+            <Input.Password placeholder='Password' />
+          </Form.Item>
+          <Form.Item
+            name='confirmPassword'
+            dependencies={['password']}
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              confirmPasswordValidator,
+            ]}
+          >
+            <Input.Password placeholder='Confirm Password' />
+          </Form.Item>
 
-        <Button htmlType='submit' className='next-btn' >Next </Button>
-        
-      </Form>
+          <Form.Item>
+            <Button htmlType='submit' className='next-btn'>
+              Next
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
